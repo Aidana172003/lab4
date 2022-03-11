0e73a2a1a44faee2de21f0e743c39c394014bf24
Aidana172003 <abdukarimova.03@list.ru>
2022-03-11 21:08:20 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hwf9pg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hwf9pg)
---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'CeraVe Moistrurizing Cream',
+    price: 16.25,
+    description: 'For face and body or can be used as a hand cream for dry skin relief.'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
